0e317e5a86a86a64d84e397782265dd64ea9d170
Filipp Gorbunov <Filippgorb@gmail.com>
2018-07-19 09:40:58 -0700

    changed secret

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ module.exports = {
   TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
+  
 };
